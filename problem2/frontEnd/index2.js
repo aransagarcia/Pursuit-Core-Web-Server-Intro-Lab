8e2050dbb21c4ffb2ca84284b2606d5cecd512bc
aransagarcia <aransagarcia@pursuit.org>
2019-10-12 18:01:15 -0400

    created options for selectbox

--- problem2/frontEnd/index2.js
+++ problem2/frontEnd/index2.js
@@ -7,9 +7,12 @@ async function loadDataFromServer() {
     dataArr = response.data.results
     // console.log("data", dataArr[7])
     displayData(dataArr)
+    console.log(dataArr)
 }
 
-displayData=(dataArr)=>{
+
+
+displayData = (dataArr) => {
     let selectedQuestion = []
     let question1 = document.createElement("p");
     selectedQuestion = dataArr[Math.floor(Math.random() * dataArr.length)]
@@ -18,17 +21,35 @@ displayData=(dataArr)=>{
     console.log(dataArr)
     console.log("question", question1)
     document.body.append(question1)
+    makeChoices(selectedQuestion)
+}
 
 
+
+makeChoices = (selectedQuestion) => {
     let selectBox = document.createElement('select')
     let option1 = document.createElement('option')
     let option2 = document.createElement('option')
-    let opttion3 = document.createElement('option')
-    
-selectBox.id = "answerChoices"
-document.body.append(selectBox)
+    let option3 = document.createElement('option')
+
+    //console.log("the array we are working with", selectedQuestion)
+
+    //option1.innerText = selectedQuestion.incorrect_answers[0]
+    option1.value = 'hello'
+    option1.appendChild.selectBox
+    option2.innerText = selectedQuestion.incorrect_answers[1]
+    option3.innerText = selectedQuestion.incorrect_answers[2]
+
+
+    //option4.innerText = selectedQuestion.correct_answer
+
+    console.log(selectBox);
+    console.log(option1);
 
 
 
+    selectBox.appendChild.option1
+    selectBox.id = "answerChoices"
+    document.body.append(selectBox)
 }
 
