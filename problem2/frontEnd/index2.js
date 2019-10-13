f9cab3492053ec54f9f8b043a25b733ff4c72ce5
aransagarcia <aransagarcia@pursuit.org>
2019-10-13 10:33:17 -0400

    started working on .change function

--- problem2/frontEnd/index2.js
+++ problem2/frontEnd/index2.js
@@ -32,20 +32,40 @@ makeChoices = (selectedQuestion) => {
     let option2 = document.createElement('option')
     let option3 = document.createElement('option')
     let option4 = document.createElement('option')
-    //console.log("the array we are working with", selectedQuestion)
+    console.log("the array we are working with", selectedQuestion)
+
 
     option1.innerText = selectedQuestion.incorrect_answers[0]
     option2.innerText = selectedQuestion.incorrect_answers[1]
     option3.innerText = selectedQuestion.incorrect_answers[2]
     option4.innerText = selectedQuestion.correct_answer
 
+
+
     selectBox.appendChild(option1)
     selectBox.appendChild(option2)
     selectBox.appendChild(option3)
     selectBox.appendChild(option4)
-    
+
     selectBox.id = "answerChoices"
     selectBox.value = "answer Choices"
     document.body.append(selectBox)
-}
 
+    let arrayOfChoices = [option1, option2, option3, option4]
+    console.log(arrayOfChoices)
+
+}
+const selectingChoice = (arrayOfChoices) => {
+    dropdown.addEventListener('change', (event) => {
+        let response = document.createElement('p')
+        for (let i = 0; i < arrayOfChoices.length; i++) {
+            if (arrayOfChoices[0] || arrayOfChoices[1] || arrayOfChoices[2]) {
+                response.innerText = "incorrect answer"
+                document.body.append(response)
+            } else {
+                response.innerText = "correct answer"
+                document.body.append(response)
+            }
+        }
+    })
+}
